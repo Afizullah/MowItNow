4f4b31d93235b32345f2a6ffa4ad85baa773bf63
Afizullah <Afizullah@users.noreply.github.com>
2019-11-21 20:12:25 +0100

    correction bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from objets import Tondeuse, Pelouse
-
 import sys
+
 
 def parse_lines(lines):
     """
@@ -14,30 +14,34 @@ def parse_lines(lines):
 
 def main():
 
-    #Lit le fichier d'entrée
+    # Lit le fichier d'entrée
     with open(sys.argv[1]) as input:
         lines = input.read().splitlines()
 
     num_tondeuse = 0
+    # limite de la carte
     lim = lines[0].split()
     lines = lines[1:]
+
     pelouse = Pelouse(*lim)
-    #liste des tondeuses
+    # liste des tondeuses
     tonds = []
 
     while(len(lines) > 1):
 
         num_tondeuse += 1
         pos_init, chemin, lines = parse_lines(lines)
-        print(f"limite carte = {lim}, position initiale = {pos_init}, chemin = {chemin}, tondeuse n°{num_tondeuse}")
+
+        print(
+            f"limite carte = {lim}, position initiale = {pos_init}, chemin = {chemin}, tondeuse n°{num_tondeuse}")
 
         tond = Tondeuse(pelouse, pos_init)
         tond.full_move(chemin)
-        
+
         print(f"Tondeuse {num_tondeuse} : {tond.X} { tond.Y} {tond.O}")
-        
+
         tonds.append(tond)
-        print("#################")
+
 
 if __name__ == "__main__":
     main()
--- objets.py
+++ objets.py
@@ -1,38 +1,48 @@
 import copy
 
+
 class Pelouse:
     def __init__(self, x, y):
+
+        X, Y = int(x), int(y)
+        assert X > 0 and Y > 0, "carte non valable"
+
         self.limX = int(x)
         self.limY = int(y)
 
+
 class Tondeuse:
-    # orientation[self.O][deplacement]
+    # orientation[self.O][deplacement] donne le sens dans lequel doit s'orienter la tondeuse
     orientation = {'N':
-                    {'G': 'W',
-                    'D': 'E'}
-                    ,
+                   {'G': 'W',
+                    'D': 'E'},
                    'S':
-                    {'G': 'E',
-                    'D': 'W'}
-                    ,
+                   {'G': 'E',
+                    'D': 'W'},
                    'E':
-                    {'G': 'N',
-                    'D': 'S'}
-                    ,
+                   {'G': 'N',
+                    'D': 'S'},
                    'W':
-                    {'G': 'S',
+                   {'G': 'S',
                     'D': 'N'}
-                    }
-        
+                   }
+
     def __init__(self, pelouse, init_pos):
+
         X, Y, O = init_pos
+        X, Y = int(X), int(Y)
+
+        assert O in ['N', 'S', 'E', 'W'], "Orientation non valide"
+        assert (0 < X < pelouse.limX) and (
+            0 < Y < pelouse.limY), "Position non valable"
+
         self.limX = pelouse.limX
         self.limY = pelouse.limY
-        self.X = int(X)
-        self.Y = int(Y)
+        self.X = X
+        self.Y = Y
         self.O = O
         self.pelouse = pelouse
-    
+
     def __str__(self):
         return f"{self.X}, {self.Y}, {self.O}"
 
@@ -40,22 +50,20 @@ class Tondeuse:
         return f"{self.X}, {self.Y}, {self.O}"
 
     def copy(self):
-        return Tondeuse(self.pelouse, (self.X, self.Y, self.O))
+        return copy.deepcopy(self)
 
     def can_move(self, deplacement):
-
+        # verifier que le déplacement est possible
         assert deplacement in ['G', 'D', 'A'], "Déplacement non valide"
         tond_bis = self.copy()
         tond_bis.move(deplacement)
-        print(f"tond = {self}")
-        print(f"tond_bid = {tond_bis}")
         return tond_bis.is_legit()
 
     def is_legit(self):
         return (0 <= self.X <= self.limX) and (0 <= self.Y <= self.limY)
 
     def move(self, deplacement):
-
+        # effectue le movement
         if deplacement == 'G':
             self.O = Tondeuse.orientation[self.O]['G']
 
@@ -65,19 +73,17 @@ class Tondeuse:
         if deplacement == 'A':
 
             if self.O == 'N':
-                self.X = self.Y + 1
+                self.Y = self.Y + 1
             if self.O == 'S':
-                self.X = self.Y - 1
+                self.Y = self.Y - 1
             if self.O == 'E':
-                self.Y = self.X + 1
+                self.X = self.X + 1
             if self.O == 'W':
-                self.Y = self.X - 1
+                self.X = self.X - 1
 
     def full_move(self, chemin):
 
         for indice, dep in enumerate(chemin):
             if (self.can_move(dep)):
-                #print(dep)
-                #self.move(dep)
-                pass
-        print(indice)
+                # print(dep)
+                self.move(dep)
