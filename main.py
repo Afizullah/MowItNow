6bd521305177b342a2e465d0be2923a0a3f45ce4
Afizullah <36815337+Afizullah@users.noreply.github.com>
2019-11-21 18:26:59 +0100

    reste juste un bug de deplacement

---
+++ main.py
@@ -0,0 +1,43 @@
+from objets import Tondeuse, Pelouse
+
+import sys
+
+def parse_lines(lines):
+    """
+    recueil la position initiale et le chemin à parcourir de la tondeuse
+    """
+    pos_init, chemin = lines[:2]
+    pos_init = pos_init.split()
+
+    return pos_init, chemin, lines[2:]
+
+
+def main():
+
+    #Lit le fichier d'entrée
+    with open(sys.argv[1]) as input:
+        lines = input.read().splitlines()
+
+    num_tondeuse = 0
+    lim = lines[0].split()
+    lines = lines[1:]
+    pelouse = Pelouse(*lim)
+    #liste des tondeuses
+    tonds = []
+
+    while(len(lines) > 1):
+
+        num_tondeuse += 1
+        pos_init, chemin, lines = parse_lines(lines)
+        print(f"limite carte = {lim}, position initiale = {pos_init}, chemin = {chemin}, tondeuse n°{num_tondeuse}")
+
+        tond = Tondeuse(pelouse, pos_init)
+        tond.full_move(chemin)
+        
+        print(f"Tondeuse {num_tondeuse} : {tond.X} { tond.Y} {tond.O}")
+        
+        tonds.append(tond)
+        print("#################")
+
+if __name__ == "__main__":
+    main()
---
+++ objets.py
@@ -0,0 +1,83 @@
+import copy
+
+class Pelouse:
+    def __init__(self, x, y):
+        self.limX = int(x)
+        self.limY = int(y)
+
+class Tondeuse:
+    # orientation[self.O][deplacement]
+    orientation = {'N':
+                    {'G': 'W',
+                    'D': 'E'}
+                    ,
+                   'S':
+                    {'G': 'E',
+                    'D': 'W'}
+                    ,
+                   'E':
+                    {'G': 'N',
+                    'D': 'S'}
+                    ,
+                   'W':
+                    {'G': 'S',
+                    'D': 'N'}
+                    }
+        
+    def __init__(self, pelouse, init_pos):
+        X, Y, O = init_pos
+        self.limX = pelouse.limX
+        self.limY = pelouse.limY
+        self.X = int(X)
+        self.Y = int(Y)
+        self.O = O
+        self.pelouse = pelouse
+    
+    def __str__(self):
+        return f"{self.X}, {self.Y}, {self.O}"
+
+    def __repr__(self):
+        return f"{self.X}, {self.Y}, {self.O}"
+
+    def copy(self):
+        return Tondeuse(self.pelouse, (self.X, self.Y, self.O))
+
+    def can_move(self, deplacement):
+
+        assert deplacement in ['G', 'D', 'A'], "Déplacement non valide"
+        tond_bis = self.copy()
+        tond_bis.move(deplacement)
+        print(f"tond = {self}")
+        print(f"tond_bid = {tond_bis}")
+        return tond_bis.is_legit()
+
+    def is_legit(self):
+        return (0 <= self.X <= self.limX) and (0 <= self.Y <= self.limY)
+
+    def move(self, deplacement):
+
+        if deplacement == 'G':
+            self.O = Tondeuse.orientation[self.O]['G']
+
+        if deplacement == 'D':
+            self.O = Tondeuse.orientation[self.O]['D']
+
+        if deplacement == 'A':
+
+            if self.O == 'N':
+                self.X = self.Y + 1
+            if self.O == 'S':
+                self.X = self.Y - 1
+            if self.O == 'E':
+                self.Y = self.X + 1
+            if self.O == 'W':
+                self.Y = self.X - 1
+
+    def full_move(self, chemin):
+
+        for indice, dep in enumerate(chemin):
+            if (self.can_move(dep)):
+                #print(dep)
+                #self.move(dep)
+                pass
+        print(indice)
